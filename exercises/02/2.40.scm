96efc6ca5704f80a93fdacb151514e53097773ac
jiacai <jiacai2050@gmail.com>
2015-07-29 22:52:11 +0800

    new note 2015-07-29

---
+++ exercises/02/2.40.scm
@@ -0,0 +1,37 @@
+(load "lib/op.scm")
+
+(define (unique-pairs n)
+  (define (enumerate-interval n)
+    (if (< n 1)
+      nil
+      (append (enumerate-interval (- n 1))
+              (list n))))
+  (accumulate append nil 
+    (map
+      (lambda (i)
+        (map (lambda (j) (list j i))
+          (enumerate-interval (- i 1))))
+      (enumerate-interval n))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(unique-pairs 4)
+;Value: ((1 2) (1 3) (2 3) (1 4) (2 4) (3 4))
+
+(define (prime? n)
+  (define (iter i)
+    (if (< i n)
+      (if (not (= 0 (remainder n i)))
+        (iter (+ i 1))
+        #f)
+      #t))
+  (iter 2))
+
+(define (prime-sum-pairs n)
+  (filter (lambda (p)
+            (prime? (+ (car p) (cadr p))))
+          (unique-pairs n)))
+
+(prime-sum-pairs 6)
+;Value: ((1 2) (2 3) (1 4) (3 4) (2 5) (1 6) (5 6))
---
+++ exercises/02/2.41.scm
@@ -0,0 +1,15 @@
+(load "2.40.scm")
+(define (s-triple n s)
+  (filter
+    (lambda (triple)
+      (let ((a1 (car triple))
+            (a2 (cadr triple))
+            (a3 (caddr triple)))
+      (and (< a1 a3)
+           (< a2 a3))))
+    (map (lambda (p) 
+           (append p (list (- s (car p) (cadr p)))))
+      (unique-pairs n))))
+
+(s-triple 10 10)
+;Value: ((1 2 7) (1 3 6) (2 3 5) (1 4 5))
