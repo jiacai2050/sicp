9f13f0da60341d2f1a19d3c907ccd5fd28237d30
jiacai2050 <jiacai2050@gmail.com>
2016-03-15 23:43:30 +0800

    add 4.35.scm

---
+++ exercises/04/4.35.scm
@@ -0,0 +1,11 @@
+(define (an-integer-between low high)
+  (require (<= low high))
+  (amb low (an-integer-between (+ low 1) high)))
+
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high)))
+    (let ((j (an-integer-between i high)))
+      (let ((k (an-integer-between j high)))
+        (require (= (+ (* i i) (* j j))
+                    (* k k)))
+        (list i j k)))))
