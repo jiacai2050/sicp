563164fbfcff6fa6dfdfde23ac692cfe1da86b5f
jiacai2050 <jiacai2050@gmail.com>
2016-04-07 22:28:18 +0800

    update amb eval

--- exercises/04/lib/amb/exp.scm
+++ exercises/04/lib/amb/exp.scm
@@ -1,3 +1,4 @@
+; 简单表达式
 (define (analyze-self-evaluating exp)
   (lambda (env succeed fail)
     (succeed exp fail)))
@@ -17,4 +18,145 @@
         (bproc (analyze-sequence (lambda-body exp))))
     (lambda (env succeed fail)
       (succeed (make-procedure vars bproc env)
-               fail))))   
+               fail))))
+
+;条件和序列
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (pred-value fail2)
+               (if (true? pred-value)
+                 (cproc env succeed fail2)
+                 (aproc env succeed fail2))
+             fail)))))
+
+(define (analyze-sequence exps)
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         (lambda (a-value fail2)
+           (b env succeed fail2))
+         fail)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+      first-proc
+      (loop (sequentially first-proc (car rest-procs))
+            (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+      (error "EMPTY sequence -- ANALYZE"))
+
+      (loop (car procs) (cdr procs))))
+
+;定义和赋值
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (define-variable! var val env))
+             fail))))
+
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (let ((old-value (lookup-variable var env)))
+                 (set-variable-value! var val env)
+                 (succeed 'ok
+                          (lambda ()
+                            (set-variable-value! var
+                                                 old-value
+                                                 env)
+                            (fail2)))))
+             fail))))
+
+;过程应用
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env succeed fail)
+      (fproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                             proc args succeed fail3))
+                         fail2))
+             fail))))
+
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+    (succeed '() fail)
+    ((car aprocs) env
+                  (lambda (arg fail2)
+                    (get-args (cdr aprocs)
+                              env
+                              (lambda (args fail3)
+                                (succeed (cons arg args)
+                                         fail3))
+                              fail2))
+                  fail)))
+
+
+(define (execute-application proc args succeed fail)
+  (cond ((primitive-procedure? proc)
+          (succeed (apply-primitive-procedure proc args)
+                   fail))
+        ((compound-procedure? proc)
+          ((procedure-body proc)
+            (extend-environment (procedure-parameters proc)
+                                args
+                                (procedure-environment proc))
+            succeed
+            fail))
+        (else
+          (error "Unknown procedure type -- EXECUTE-APPLICATION" proc))))
+
+;amb 表达式的求值
+
+(define (analyze-amb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+          (fail)
+          ((car choices) env
+                         succeed
+                         (lambda ()
+                           (try-next (cdr choices))))))
+      (try-next cprocs))))
+
+(define input-prompt " Amb-Eval input: ")
+(define output-prompt " Amb-Eval values: ")
+(define (driver-loop)
+  (define (internal-loop try-again)
+    (prompt-for-input input-prompt)
+    (let ((input (read)))
+      (if (eq? input 'try-again)
+        (try-again)
+        (begin
+          (newline)
+          (display "Starting a new problem ")
+          (ambeval input
+                   the-global-environment
+                   (lambda (val enxt-alternative)
+                     (announce-output output-prompt)
+                     (user-print val)
+                     (internal-loop next-alternative))
+                   (lambda ()
+                     (announce-output "There are no more values of")
+                     (user-print input)
+                     (driver-loop)))))))
+  (internal-loop
+    (lambda ()
+      (newline)
+      (display "There is no current problem")
+      (driver-loop))))   
